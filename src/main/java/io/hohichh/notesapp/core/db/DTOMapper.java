a2e662ab636d03d44c981f4617768570f7707b8e
Hohichh <verkovich2005@gmail.com>
2025-05-26 22:30:58 +0300

    start dtomapper

--- src/main/java/io/hohichh/notesapp/core/db/DTOMapper.java
+++ src/main/java/io/hohichh/notesapp/core/db/DTOMapper.java
@@ -1,4 +1,22 @@
 package io.hohichh.notesapp.core.db;
 
+import io.hohichh.notesapp.core.model.Media;
+import io.hohichh.notesapp.core.model.Note;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class DTOMapper {
+    public static void noteToStatement(final Note note, final Statement statement) {
+
+    }
+    public static Note resultSetToNote(final ResultSet resultSet) {
+        return null;
+    }
+    public static void mediaToStatement(final Media media, final Statement statement) {
+
+    }
+    public static Media resultSetToMedia(final ResultSet resultSet) {
+        return null;
+    }
 }
