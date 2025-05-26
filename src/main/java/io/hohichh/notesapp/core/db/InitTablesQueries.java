508496c4cc6b975ae845a267c6c0b9127df8aa63
Hohichh <verkovich2005@gmail.com>
2025-05-26 17:06:52 +0300

    define init tables queries literals

---
+++ src/main/java/io/hohichh/notesapp/core/db/InitTablesQueries.java
@@ -0,0 +1,18 @@
+package io.hohichh.notesapp.core.db;
+
+public class InitTablesQueries {
+    public static final String CREATE_NOTES_TABLE =
+            "CREATE TABLE IF NOT EXISTS notes ("
+            + "id TEXT NOT NULL PRIMARY KEY, "
+            + "title TEXT NOT NULL, "
+            + "content TEXT NOT NULL, "
+            + "created_at INTEGER NOT NULL, "
+            + "updated_at INTEGER NOT NULL );";
+
+    public static final String CREATE_IMAGES_TABLE =
+            "CREATE TABLE IF NOT EXISTS images ("
+            + "id TEXT NOT NULL PRIMARY KEY, "
+            + "note_id TEXT NOT NULL, "
+            + "path TEXT NOT NULL, "
+            + "insert_label TEXT NOT NULL );";
+}
