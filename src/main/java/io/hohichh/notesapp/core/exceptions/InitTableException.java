3fb115c62b5a8a4f817acdbd732680cd34ba4662
Hohichh <verkovich2005@gmail.com>
2025-05-26 17:07:29 +0300

    init table exception

---
+++ src/main/java/io/hohichh/notesapp/core/exceptions/InitTableException.java
@@ -0,0 +1,7 @@
+package io.hohichh.notesapp.core.exceptions;
+
+public class InitTableException extends StorageException{
+    public InitTableException(String message) {
+        super(message);
+    }
+}
