cce839c1ec550a1c0bb703cfa2b32db4568c2a45
Hohichh <verkovich2005@gmail.com>
2025-08-01 16:22:07 +0300

    change get and delete method's parameters

--- src/main/java/io/hohichh/notesapp/core/Notebook.java
+++ src/main/java/io/hohichh/notesapp/core/Notebook.java
@@ -3,11 +3,12 @@ package io.hohichh.notesapp.core;
 import io.hohichh.notesapp.core.model.Note;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface Notebook {
     public void createNote(Note note);
     public void updateNote(Note note);
-    public Note getNote(String id);
+    public Note getNote(UUID id);
     public List<Note> getAllNotes();
-    public void deleteNote(String id);
+    public void deleteNote(UUID id);
 }
