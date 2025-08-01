3d3009db9c37630e42acfa9db18341b3d359daa2
Hohichh <verkovich2005@gmail.com>
2025-08-01 16:16:14 +0300

    finalize Repository.java

--- src/main/java/io/hohichh/notesapp/core/db/sqlite/Repository.java
+++ src/main/java/io/hohichh/notesapp/core/db/sqlite/Repository.java
@@ -3,10 +3,10 @@ package io.hohichh.notesapp.core.db.sqlite;
 import io.hohichh.notesapp.core.db.IMediaDAO;
 import io.hohichh.notesapp.core.db.INoteDAO;
 import io.hohichh.notesapp.core.db.IRepository;
-import io.hohichh.notesapp.core.exceptions.SqliteRepException;
+import io.hohichh.notesapp.core.exceptions.RepositoryException;
+import io.hohichh.notesapp.core.model.Media;
 import io.hohichh.notesapp.core.model.Note;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
@@ -17,8 +17,8 @@ import static io.hohichh.notesapp.core.db.queries.InitTablesQueries.CREATE_NOTES
 
 
 public class Repository implements IRepository {
-    private INoteDAO noteDAO;
-    private IMediaDAO mediaDAO;
+    private final INoteDAO noteDAO;
+    private final IMediaDAO mediaDAO;
 
     public Repository(INoteDAO noteDAO, IMediaDAO mediaDAO)  {
         this.noteDAO = noteDAO;
@@ -26,55 +26,66 @@ public class Repository implements IRepository {
         try {
             initTables();
         } catch (SQLException e) {
-            throw new SqliteRepException("Can't create tables: " + e.getMessage(), e);
+            throw new RepositoryException("Can't create tables: " + e.getMessage(), e);
         }
     }
 
     @Override
     public void create(Note note)  {
         try{
-
+            noteDAO.create(note);
+            mediaDAO.create(note.getMediaContent());
         }catch (SQLException e){
-
+            throw new RepositoryException("\nCan't create note: " + e.getMessage(), e);
         }
     }
 
     @Override
     public void delete(UUID id)  {
         try{
-
+            mediaDAO.delete(id);
+            noteDAO.delete(id);
         }catch (SQLException e){
-
+            throw new RepositoryException("Can't delete note: " + e.getMessage(), e);
         }
     }
 
     @Override
     public void update(Note note)  {
         try{
-
+            mediaDAO.delete(note.getId());
+            noteDAO.update(note);
+            mediaDAO.create(note.getMediaContent());
         }catch (SQLException e){
-
+            throw new RepositoryException("Can't update note: " + e.getMessage(), e);
         }
     }
 
     @Override
     public Note get(UUID id)  {
+        Note note = null;
         try{
+            var listOfMedia = mediaDAO.getAll(id);
+            note = noteDAO.get(id);
 
+            for(Media media: listOfMedia){
+                note.addMedia(media);
+            }
         }catch (SQLException e){
-
+            throw new RepositoryException("Can't get note: " + e.getMessage(), e);
         }
-        return null;
+        return note;
     }
 
     @Override
     public List<Note> getAll()  {
+        List<Note> notes = null;
         try{
-
+            notes = noteDAO.getAll();
         }catch (SQLException e){
-
+            throw new RepositoryException("Can't get list of notes: " + e.getMessage(), e);
         }
-        return List.of();
+        return notes;
     }
 
     private void initTables() throws SQLException {
