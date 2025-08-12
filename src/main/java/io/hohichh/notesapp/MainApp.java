1724b47261bca7ca10ab0ab33a02ca50297230ce
Hohichh <verkovich2005@gmail.com>
2025-08-12 20:48:23 +0300

    add fxml main page markup

--- src/main/java/io/hohichh/notesapp/MainApp.java
+++ src/main/java/io/hohichh/notesapp/MainApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
@@ -13,7 +14,9 @@ public class MainApp extends Application {
         System.out.println("started");
         Label hello = new Label("Hello, JavaFX!");
         StackPane root = new StackPane(hello);
-
+        // Загружаем один или несколько стилей шрифта
+        Font.loadFont(getClass().getResourceAsStream("/fonts/Montserrat-Regular.ttf"), 16);
+        Font.loadFont(getClass().getResourceAsStream("/fonts/Montserrat-Bold.ttf"), 16);
         Scene scene = new Scene(root, 800, 600);
 
         stage.setTitle("Desktop Notes");
